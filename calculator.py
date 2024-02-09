4f53f74af0eb167095ee026a0a61e53b7a65b6e9
victor lopez <lopezvictorfabian74@gmail.com>
2024-02-09 18:07:13 -0500

    add multiplication function

--- calculator.py
+++ calculator.py
@@ -2,7 +2,10 @@ def add(num_1, num_2):
     return num_1 + num_2
 
 def substract(num_1, num_2):
-    return num_1 + num_2
+    return num_1 - num_2
 
 def division(num_1, num_2):
-    return num_1 + num_2
+    return num_1 / num_2
+
+def multiplication(num_1, num_2):
+    return num_1 * num_2
